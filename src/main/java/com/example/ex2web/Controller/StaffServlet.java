6eca848aa25f96559c1bb12b502e8035f77cac19
sheep <samcoxe0307@gmail.com>
2024-12-10 15:11:43 +0700

    ??

--- src/main/java/com/example/ex2web/Controller/StaffServlet.java
+++ src/main/java/com/example/ex2web/Controller/StaffServlet.java
@@ -1,5 +1,10 @@
 package com.example.ex2web.Controller;
 
+import com.example.ex2web.Model.Staff;
+import com.example.ex2web.Service.EmployeeService;
+import com.example.ex2web.Service.EmployeeServiceImpl;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,17 +12,74 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+
 @WebServlet(name = "StaffServlet", value = "/staffs")
 
 public class StaffServlet extends HttpServlet {
+    private final EmployeeService employeeService = new EmployeeServiceImpl();
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
 
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "add":
+                addStaff(req, resp);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void addStaff(HttpServletRequest req, HttpServletResponse resp) {
+        String name = req.getParameter("name");
+        int age = Integer.parseInt(req.getParameter("age"));
+        String position = req.getParameter("position");
+        String department = req.getParameter("department");
+        double salary = Double.parseDouble(req.getParameter("salary"));
+        int id = (int) (Math.random() * 10000);
+
+        Staff staff = new Staff(id, name, age, position, department, salary);
+        this.employeeService.addStaff(staff);
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("staff/add.jsp");
+        req.setAttribute("message", "New customer was added successfully");
+
+        try {
+            dispatcher.forward(req, resp);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String action = req.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            case "add":
+                showAddForm(req, resp);
+                break;
+        }
+    }
 
+    private void showAddForm(HttpServletRequest req, HttpServletResponse resp) {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("staff/add.jsp");
+
+        try {
+            dispatcher.forward(req, resp);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
+
 
